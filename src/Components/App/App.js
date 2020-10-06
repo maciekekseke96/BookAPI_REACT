4cb93df26afda874a0d023411888ba454c45af31
maciekekseke96 <maciek20145c@wp.pl>
2020-10-06 14:32:13 +0200

    Iniital commit. New fresh project

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>New BookAPI Project works ?</h1>
+    </div>
+  );
+}
+
+export default App;
